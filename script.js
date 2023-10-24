35cf1f97fd0fb6f308ed16325cd92ae33c0b4a33
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2023-10-24 22:31:58 +0200

    added hover mouse functionality, color buttons and reset

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function color(){
-    console.log("test");
+let paintColor = 'black';
+function color(pixel){
+    pixel.setAttribute('style','background-color: '+ paintColor + ';');
+}
+
+function reset(){
+    pixels = document.querySelectorAll("#pixel");
+    console.log(1);
+    pixels.forEach(element => {
+        element.setAttribute('style','background-color: white;')   
+    });
+
 }
 
 
-let size = 32;
-const pixel = document.createElement("div");
-pixel.id = "pixel";
-pixel.addEventListener('click',color);
 
+
+let size = 32;
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener('click',reset)
+const blackButton = document.getElementById("black");
+blackButton.addEventListener('click',function(e){paintColor = 'black'})
+const redButton = document.getElementById("red");
+redButton.addEventListener('click',function(e){paintColor = 'red';})
+const whiteButton = document.getElementById("white");
+whiteButton.addEventListener('click',function(e){paintColor = 'white'})
 
 const square = document.createElement("div");
-
-
-
 const applicationWindow = document.querySelector("#applicationWindow");
 
 for (let i = 0; i<size; i++){
@@ -23,7 +36,9 @@ for (let i = 0; i<size; i++){
     {
         const pixel = document.createElement("div");
         pixel.id = "pixel";
-        pixel.addEventListener('click',color);
+        pixel.addEventListener('mouseover',function(){
+            color(this);
+        });
         row.appendChild(pixel);
     }
     applicationWindow.appendChild(row);
